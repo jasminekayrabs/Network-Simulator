3b1cfd1d3abfb290b6698df354ed2d9a1ea29cfe
Sara mojadami <97891856+saramoj@users.noreply.github.com>
2024-01-13 15:28:10 +0100

    Update NetworkConnection.java

--- JDP-ICA1/src/cz/pcu/NetworkConnection.java
+++ JDP-ICA1/src/cz/pcu/NetworkConnection.java
@@ -4,6 +4,7 @@ public interface NetworkConnection {
     boolean isActive();
     Device getDevice1();
     Device getDevice2();
-
     void transferData();
+    void handleEvent(String event);
+    void addConnection(Device device1, Device device2);
 }
