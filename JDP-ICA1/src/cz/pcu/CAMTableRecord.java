cd8a624df316e5baf653189abe018a6c7de3c411
Veronikaktsv <97884440+Veronikaktsv@users.noreply.github.com>
2024-01-12 22:18:15 -0800

    Create CAMTableRecord.java

---
+++ JDP-ICA1/src/cz/pcu/CAMTableRecord.java
@@ -0,0 +1,30 @@
+package cz.pcu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class CAMTableRecord {
+    private String macAddress;
+    private NetworkInterface port;
+
+    public CAMTableRecord(String macAddress, NetworkInterface port) {
+        this.macAddress = macAddress;
+        this.port = port;
+    }
+
+    public String getMacAddress() {
+        return macAddress;
+    }
+
+    public void setMacAddress(String macAddress) {
+        this.macAddress = macAddress;
+    }
+
+    public NetworkInterface getPort() {
+        return port;
+    }
+
+    public void setPort(NetworkInterface port) {
+        this.port = port;
+    }
+}
