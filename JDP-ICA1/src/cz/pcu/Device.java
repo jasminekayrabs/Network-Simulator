ec9e89e2c9fc9f69188a53ddf5690a572c6c4a6a
Veronikaktsv <97884440+Veronikaktsv@users.noreply.github.com>
2024-01-12 22:32:33 -0800

    Update Device.java

--- JDP-ICA1/src/cz/pcu/Device.java
+++ JDP-ICA1/src/cz/pcu/Device.java
@@ -1,13 +1,18 @@
 package cz.pcu;
+import java.util.List;
 
 abstract class Device {
-    int id;
-    String name;
-    String macAddress;
-    List<NetworkInterface> interfaces;
-    NetworkProtocol protocol; 
+    private int id;
+    private String name;
+    private String macAddress;
+    private List<NetworkInterface> interfaces;
+    private NetworkProtocol protocol; 
 
-    public Device(NetworkProtocol protocol) {
+    public Device(int id, String name, String macAddress, List<NetworkInterface> interfaces, NetworkProtocol protocol) {
+        this.id = id;
+        this.name = name;
+        this.macAddress = macAddress;
+        this.interfaces = interfaces;
         this.protocol = protocol;
     }
 }
