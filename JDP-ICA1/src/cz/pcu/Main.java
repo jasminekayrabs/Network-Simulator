c3b7e7e16dbb5b0b85f478b44f1cd5a285c02f8c
Polina Krasnianska <p.krasniansky@gmail.com>
2024-01-13 23:52:24 +0100

    application's main method

--- JDP-ICA1/src/cz/pcu/Main.java
+++ JDP-ICA1/src/cz/pcu/Main.java
@@ -1,7 +1,14 @@
 package cz.pcu;
 
+import cz.pcu.simulation.SimulationManager;
+import cz.pcu.simulation.event.EventScheduler;
+
 public class Main {
     public static void main(String[] args) {
 
+        var simulationManager = SimulationManager.getInstance();
+        simulationManager.setEventScheduler(new EventScheduler());
+        simulationManager.buildNetwork("path to topology file");
+        simulationManager.startSimulation();
     }
 }
